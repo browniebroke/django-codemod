5b6756be2dc959c348bf32a6fa2872120f2bbf62
Bruno Alla <alla.brunoo@gmail.com>
2020-05-06 18:11:54 +0100

    Tweaks from Boilerplate

--- docs/installation.rst
+++ docs/installation.rst
@@ -12,7 +12,7 @@ To install Django Codemod, run this command in your terminal:
 
 .. code-block:: console
 
-    $ pip install django_codemod
+    $ pip install django-codemod
 
 This is the preferred method to install Django Codemod, as it will always install the most recent stable release.
 
@@ -32,13 +32,13 @@ You can either clone the public repository:
 
 .. code-block:: console
 
-    $ git clone git://github.com/browniebroke/django_codemod
+    $ git clone git://github.com/browniebroke/django-codemod
 
 Or download the `tarball`_:
 
 .. code-block:: console
 
-    $ curl -OJL https://github.com/browniebroke/django_codemod/tarball/master
+    $ curl -OJL https://github.com/browniebroke/django-codemod/tarball/master
 
 Once you have a copy of the source, you can install it with:
 
@@ -47,5 +47,5 @@ Once you have a copy of the source, you can install it with:
     $ python setup.py install
 
 
-.. _Github repo: https://github.com/browniebroke/django_codemod
-.. _tarball: https://github.com/browniebroke/django_codemod/tarball/master
+.. _Github repo: https://github.com/browniebroke/django-codemod
+.. _tarball: https://github.com/browniebroke/django-codemod/tarball/master
--- setup.py
+++ setup.py
@@ -10,23 +10,22 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=7.0', ]
+requirements = ['Click>=7.0']
 
-setup_requirements = ['pytest-runner', ]
+setup_requirements = ['pytest-runner']
 
-test_requirements = ['pytest>=3', ]
+test_requirements = ['pytest>=3']
 
 setup(
     author="Bruno Alla",
     author_email='alla.brunoo@gmail.com',
-    python_requires='>=3.5',
+    python_requires='>=3.6',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
@@ -41,13 +40,13 @@ setup(
     license="MIT license",
     long_description=readme + '\n\n' + history,
     include_package_data=True,
-    keywords='django_codemod',
-    name='django_codemod',
+    keywords='django,codemod',
+    name='django-codemod',
     packages=find_packages(include=['django_codemod', 'django_codemod.*']),
     setup_requires=setup_requirements,
     test_suite='tests',
     tests_require=test_requirements,
-    url='https://github.com/browniebroke/django_codemod',
+    url='https://github.com/browniebroke/django-codemod',
     version='0.1.0',
     zip_safe=False,
 )
