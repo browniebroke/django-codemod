84c0296e55fa015ae73e41d2f70062d13162aa6c
Bruno Alla <bruno.alla@festicket.com>
2020-05-06 19:34:14 +0100

    Add libcst as dependency

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=7.0']
+requirements = ['Click>=7.0', 'libcst']
 
 setup_requirements = ['pytest-runner']
 
