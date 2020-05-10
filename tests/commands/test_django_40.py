c43817faf7a5ac853ae15c47be446eac85de6b72
Bruno Alla <alla.brunoo@gmail.com>
2020-05-10 11:50:11 +0100

    Support resolving django.utils.translation.ugettext deprecation

--- tests/commands/test_django_40.py
+++ tests/commands/test_django_40.py
@@ -3,6 +3,7 @@ from libcst.codemod import CodemodTest
 from django_codemod.commands.django_40 import (
     ForceTextToStrCommand,
     SmartTextToStrCommand,
+    UGetTextToGetTextCommand,
 )
 
 
@@ -142,5 +143,55 @@ class TestSmartTextToStrCommand(CodemodTest):
         """
         after = """
             (lambda x: x)(something)
+        """
+        self.assertCodemod(before, after)
+
+
+class TestUGetTextToGetTextCommand(CodemodTest):
+
+    TRANSFORM = UGetTextToGetTextCommand
+
+    def test_noop(self) -> None:
+        """Test when nothing should change."""
+        before = """
+            from django import conf
+            from django.utils import translation
+
+            foo = gettext("bar")
+        """
+        after = """
+            from django import conf
+            from django.utils import translation
+
+            foo = gettext("bar")
+        """
+
+        self.assertCodemod(before, after)
+
+    def test_simple_substitution(self) -> None:
+        """Check simple use case."""
+        before = """
+            from django.utils.translation import ugettext
+
+            result = ugettext(content)
+        """
+        after = """
+            from django.utils.translation import gettext
+
+            result = gettext(content)
+        """
+        self.assertCodemod(before, after)
+
+    def test_str_already_imported_substitution(self) -> None:
+        """Test case where gettext is already in the imports."""
+        before = """
+            from django.utils.translation import ugettext, gettext
+
+            result = ugettext(content)
+        """
+        after = """
+            from django.utils.translation import gettext
+
+            result = gettext(content)
         """
         self.assertCodemod(before, after)
