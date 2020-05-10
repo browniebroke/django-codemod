264eaf12b8658c76b33cd51afab8747dd4338b22
Bruno Alla <alla.brunoo@gmail.com>
2020-05-10 12:45:51 +0100

    Create layout for Django 3.x codemods

---
+++ django_codemod/commands/django_31.py
@@ -0,0 +1,6 @@
+"""
+Module to fix things removed in Django 3.1.
+
+This is expected to cover most of the things listed in this section:
+https://docs.djangoproject.com/en/stable/internals/deprecation/#deprecation-removed-in-3-1
+"""
---
+++ django_codemod/commands/django_32.py
@@ -0,0 +1,6 @@
+"""
+Module to fix things removed in Django 3.2.
+
+This is expected to cover most of the things listed in this section:
+https://docs.djangoproject.com/en/stable/internals/deprecation/#deprecation-removed-in-3-2
+"""
