558c1647d7bada0d61ba67de301bf4c9ca98555b
Bruno Alla <alla.brunoo@gmail.com>
2020-05-10 12:51:18 +0100

    Bump version: 0.1.1 → 0.2.0

--- setup.py
+++ setup.py
@@ -39,6 +39,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/browniebroke/django-codemod",
-    version="0.1.1",
+    version="0.2.0",
     zip_safe=False,
 )
