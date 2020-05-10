f0a50b620843652614105e0f9eb25e97e35ef1c0
Bruno Alla <alla.brunoo@gmail.com>
2020-05-10 12:44:04 +0100

    Update docstring

--- django_codemod/commands/django_40.py
+++ django_codemod/commands/django_40.py
@@ -2,7 +2,7 @@
 Module to fix things removed in Django 4.0.
 
 This is expected to cover most of the things listed in this section:
-https://docs.djangoproject.com/en/3.0/internals/deprecation/#deprecation-removed-in-4-0
+https://docs.djangoproject.com/en/stable/internals/deprecation/#deprecation-removed-in-4-0
 """
 from abc import ABC
 from typing import Union
