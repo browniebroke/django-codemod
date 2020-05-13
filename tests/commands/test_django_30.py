253efc1baaa52a3ec9962fbef24a29028b2308f1
Bruno Alla <alla.brunoo@gmail.com>
2020-05-13 18:07:22 +0100

    New commands to fix all deprecations for a given version of Django

--- tests/commands/test_django_30.py
+++ tests/commands/test_django_30.py
@@ -1,6 +1,9 @@
 from libcst.codemod import CodemodTest
 
-from django_codemod.commands.django_30 import RenderToResponseToRenderCommand
+from django_codemod.commands.django_30 import (
+    RenderToResponseToRenderCommand,
+    Django30Command,
+)
 
 
 class TestRenderToResponseToRenderCommand(CodemodTest):
@@ -47,5 +50,23 @@ class TestRenderToResponseToRenderCommand(CodemodTest):
             from django.shortcuts import get_object_or_404, render
 
             result = render(None, "index.html", context={})
+        """
+        self.assertCodemod(before, after)
+
+
+class TestDjango30Command(CodemodTest):
+    TRANSFORM = Django30Command
+
+    def test_shortcuts_substitution(self) -> None:
+        """Check replacement of django.shortcuts."""
+        before = """
+            from django.shortcuts import render_to_response
+
+            result = render_to_response("index.html", context={}, status=None)
+        """
+        after = """
+            from django.shortcuts import render
+
+            result = render(None, "index.html", context={}, status=None)
         """
         self.assertCodemod(before, after)
--- tests/commands/test_django_40.py
+++ tests/commands/test_django_40.py
@@ -9,6 +9,7 @@ from django_codemod.commands.django_40 import (
     UNGetTextToNGetTextCommand,
     UNGetTextLazyToNGetTextLazyCommand,
     URLToRePathCommand,
+    Django40Command,
 )
 
 
@@ -446,5 +447,53 @@ class TestURLToRePathCommand(CodemodTest):
                 re_path(r'^index/$', views.index, name='index'),
                 re_path(r'^weblog/', include('blog.urls')),
             ]
+        """
+        self.assertCodemod(before, after)
+
+
+class TestDjango40Command(CodemodTest):
+
+    TRANSFORM = Django40Command
+
+    def test_url_replace_substitution(self) -> None:
+        """Check replacement of url()."""
+        before = """
+            from django.urls import include
+            from django.conf.urls import url
+
+            urlpatterns = [
+                url(r'^index/$', views.index, name='index'),
+                url(r'^weblog/', include('blog.urls')),
+            ]
+        """
+        after = """
+            from django.urls import re_path, include
+
+            urlpatterns = [
+                re_path(r'^index/$', views.index, name='index'),
+                re_path(r'^weblog/', include('blog.urls')),
+            ]
+        """
+        self.assertCodemod(before, after)
+
+    def test_utils_substitution(self) -> None:
+        """Check replacement of django.utils."""
+        before = """
+            from django.utils.encoding import force_text, smart_text
+            from django.utils.translation import ugettext, ugettext_lazy
+
+            text1 = force_text(content)
+            text2 = smart_text(content)
+            trans1 = ugettext(content)
+            trans3 = ugettext_lazy(content)
+        """
+        after = """
+            from django.utils.encoding import force_str, smart_str
+            from django.utils.translation import gettext, gettext_lazy
+
+            text1 = force_str(content)
+            text2 = smart_str(content)
+            trans1 = gettext(content)
+            trans3 = gettext_lazy(content)
         """
         self.assertCodemod(before, after)
