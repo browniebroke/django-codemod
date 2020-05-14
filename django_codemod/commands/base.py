52ef16a68c09beb8d933acba68c586bd4a43bfde
Bruno Alla <alla.brunoo@gmail.com>
2020-05-14 19:39:28 +0100

    Fix commands non-findable by libCST

---
+++ django_codemod/commands/base.py
@@ -0,0 +1,17 @@
+from abc import ABC
+from typing import List
+
+import libcst as cst
+from libcst.codemod import VisitorBasedCodemodCommand, ContextAwareTransformer
+
+
+class BaseCodemodCommand(VisitorBasedCodemodCommand, ABC):
+    """Base class for our commands."""
+
+    transformers: List[ContextAwareTransformer]
+
+    def transform_module_impl(self, tree: cst.Module) -> cst.Module:
+        for transform in self.transformers:
+            inst = transform(self.context)
+            tree = inst.transform_module(tree)
+        return tree
