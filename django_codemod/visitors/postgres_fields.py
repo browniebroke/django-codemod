7ca1c999c26ab3b2752d3542dc9a37c8a2f25f4f
Aarni Koskela <akx@iki.fi>
2020-10-14 12:41:06 +0300

    feat: add a transformer for the new core JSONField in Django 3.1

--- django_codemod/visitors/postgres_fields.py
+++ django_codemod/visitors/postgres_fields.py
@@ -1,4 +1,4 @@
-from django_codemod.constants import DJANGO_2_2, DJANGO_3_1
+from django_codemod.constants import DJANGO_2_2, DJANGO_3_1, DJANGO_4_0
 from django_codemod.visitors.base import BaseFuncRenameTransformer
 
 
@@ -18,3 +18,12 @@ class FloatRangeFormFieldTransformer(BaseFuncRenameTransformer):
     removed_in = DJANGO_3_1
     rename_from = "django.contrib.postgres.forms.FloatRangeField"
     rename_to = "django.contrib.postgres.forms.DecimalRangeField"
+
+
+class JSONModelFieldTransformer(BaseFuncRenameTransformer):
+    """Replace postgres' JSON field with the core JSON field."""
+
+    deprecated_in = DJANGO_3_1
+    removed_in = DJANGO_4_0
+    rename_from = "django.contrib.postgres.fields.JSONField"
+    rename_to = "django.db.models.JSONField"
--- tests/visitors/test_postgres_fields.py
+++ tests/visitors/test_postgres_fields.py
@@ -1,12 +1,12 @@
 from django_codemod.visitors import (
     FloatRangeFormFieldTransformer,
     FloatRangeModelFieldTransformer,
+    JSONModelFieldTransformer,
 )
 from tests.visitors.base import BaseVisitorTest
 
 
 class TestFloatRangeModelFieldTransformer(BaseVisitorTest):
-
     transformer = FloatRangeModelFieldTransformer
 
     def test_noop(self) -> None:
@@ -36,7 +36,6 @@ class TestFloatRangeModelFieldTransformer(BaseVisitorTest):
 
 
 class TestFloatRangeFormFieldTransformer(BaseVisitorTest):
-
     transformer = FloatRangeFormFieldTransformer
 
     def test_noop(self) -> None:
@@ -60,6 +59,35 @@ class TestFloatRangeFormFieldTransformer(BaseVisitorTest):
             from django.contrib.postgres.forms import DecimalRangeField
 
             some_range = DecimalRangeField()
+        """
+
+        self.assertCodemod(before, after)
+
+
+class TestJSONModelFieldTransformer(BaseVisitorTest):
+    transformer = JSONModelFieldTransformer
+
+    def test_noop(self) -> None:
+        """Test when nothing should change."""
+        before = after = """
+            from django.db.models import JSONField
+
+            field = JSONField()
+        """
+
+        self.assertCodemod(before, after)
+
+    def test_basic_replacement(self) -> None:
+        """Test simple replacement."""
+        before = """
+            from django.contrib.postgres.fields import JSONField
+
+            field = JSONField()
+        """
+        after = """
+            from django.db.models import JSONField
+
+            field = JSONField()
         """
 
         self.assertCodemod(before, after)
