f55357827a1aa6a8745f0d2f2792d8c6e53874cb
Bruno Alla <browniebroke@users.noreply.github.com>
2021-09-11 12:10:01 +0100

    feat: support for `BoundField` compatibility import removal

--- tests/visitors/test_forms.py
+++ tests/visitors/test_forms.py
@@ -1,4 +1,4 @@
-from django_codemod.visitors import PrettyNameTransformer
+from django_codemod.visitors import BoundFieldTransformer, PrettyNameTransformer
 from tests.visitors.base import BaseVisitorTest
 
 
@@ -16,5 +16,23 @@ class TestPrettyNameTransformer(BaseVisitorTest):
             from django.forms.utils import pretty_name
 
             pretty_name()
+        """
+        self.assertCodemod(before, after)
+
+
+class TestBoundFieldTransformer(BaseVisitorTest):
+
+    transformer = BoundFieldTransformer
+
+    def test_simple_substitution(self) -> None:
+        before = """
+            from django.forms.forms import BoundField
+
+            BoundField()
+        """
+        after = """
+            from django.forms.boundfield import BoundField
+
+            BoundField()
         """
         self.assertCodemod(before, after)
