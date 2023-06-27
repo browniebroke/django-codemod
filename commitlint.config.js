c44f9d697ceeb713ca38e1469f2630f03a941ca2
Bruno Alla <alla.brunoo@gmail.com>
2023-06-27 12:29:24 +0100

    chore: update commitlint config to be more permissive
    Committed via https://github.com/asottile/all-repos

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
-    "body-max-line-length": [0, "always", Infinity],
     "header-max-length": [0, "always", Infinity],
+    "body-max-line-length": [0, "always", Infinity],
+    "footer-max-line-length": [0, "always", Infinity],
   },
 };
