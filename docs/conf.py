e53bafd2e1b9ac598193d03c5f22e0da9d00f412
Bruno Alla <browniebroke@users.noreply.github.com>
2023-07-12 13:21:09 +0200

    docs: switch to furo theme (#838)
    * docs: switch to furo theme
    
    Committed via https://github.com/asottile/all-repos
    
    * docs: customise light & dark theme logos
    
    * docs: remove custom title

--- docs/conf.py
+++ docs/conf.py
@@ -87,21 +87,21 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme"
+html_theme = "furo"
 
 # Theme options are theme-specific and customize the look and feel of a
-# theme further.  For a list of options available for each theme, see the
-# documentation.
-#
-html_theme_options = {"logo_only": True}
+# theme further.
+html_theme_options = {
+    "light_logo": "img/logo-black.svg",
+    "dark_logo": "img/logo-white.svg",
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
 
-# Logo and favicon
-html_logo = "_static/img/logo-white.svg"
+# Favicon
 html_favicon = "_static/img/favicon.ico"
 
 
