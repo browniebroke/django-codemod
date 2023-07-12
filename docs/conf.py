6ebb7d2fd99823dc766a5796d11e7372cc77cd57
Bruno Alla <alla.brunoo@gmail.com>
2023-07-12 12:15:12 +0100

    docs: customise light & dark theme logos

--- docs/conf.py
+++ docs/conf.py
@@ -90,19 +90,22 @@ todo_include_todos = False
 html_theme = "furo"
 
 # Theme options are theme-specific and customize the look and feel of a
-# theme further.  For a list of options available for each theme, see the
-# documentation.
-#
-html_theme_options = {"logo_only": True}
+# theme further.
+html_theme_options = {
+    "light_logo": "img/logo-black.svg",
+    "dark_logo": "img/logo-white.svg",
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
 
-# Logo and favicon
-html_logo = "_static/img/logo-white.svg"
+# Favicon
 html_favicon = "_static/img/favicon.ico"
+
+# Customise title to avoid repeating the project name
+html_title = f"v{version}"
 
 
 # -- Options for HTMLHelp output ---------------------------------------
